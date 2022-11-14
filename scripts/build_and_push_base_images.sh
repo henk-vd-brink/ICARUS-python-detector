01b5829091a783c83a4693a10aae90bf06534299
Prominendt <hcebrink@gmail.com>
2022-11-14 07:39:01 +0100

    wip

--- scripts/build_and_push_base_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.build.yaml build icarus-base-image
-docker-compose -f docker-compose.build.yaml build icarus-tensorflow-image
-docker-compose -f docker-compose.build.yaml build icarus-pytorch-image
-
-docker-compose -f docker-compose.build.yaml build icarus-edge-detector
-
-docker-compose -f docker-compose.build.yaml push
--- scripts/build_and_run_dev.sh
+++ scripts/build_and_run_dev.sh
@@ -5,6 +5,6 @@ docker-compose -f docker-compose.build.yaml build icarus-edge-detector
 docker run -it \
     --privileged \
     --network host \
-    -v /home/jetson/repos/ICARUS-python-detector/icarus-edge-detector/src/:/home/docker_user/src \
-    -v /home/jetson/repos/ICARUS-python-detector/icarus-edge-detector/assets/:/home/docker_user/assets \
+    -v ${PWD}/icarus-edge-detector/src/:/home/docker_user/src \
+    -v ${PWD}/icarus-edge-detector/assets/:/home/docker_user/assets \
     cricarusprod001.azurecr.io/icarus-edge-detector bash
