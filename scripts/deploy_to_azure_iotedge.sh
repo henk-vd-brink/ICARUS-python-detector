e9459ce11a1bb53432185be13c614009b014d3f8
Henk <hcebrink@gmail.com>
2022-11-30 08:55:58 +0100

    Added initial set-up deployment

--- scripts/deploy_to_azure_iotedge.sh
+++ scripts/deploy_to_azure_iotedge.sh
@@ -4,13 +4,19 @@ export BUILD_VERSION=$1
 export DEPLOYMENT_VERSION=$2
 
 export AZ_IOTHUB_NAME="iot-icarus-prod-westeu-001"
-export AZ_TMP_DEPLOYMENT_FILE_PATH="/tmp/deployment_${DEPLOYMENT_VERSION}.json"
+export AZ_TMP_DEPLOYMENT_FILE_PATH="deployment_${DEPLOYMENT_VERSION}.json"
 
 pip3 install -r scripts/requirements.cd.txt
 
 python3 scripts/python/build_deployment_manifest.py
 
 az config set extension.use_dynamic_install=yes_without_prompt
+
+echo $DEPLOYMENT_VERSION
+echo $AZ_IOTHUB_NAME
+echo $AZ_TMP_DEPLOYMENT_FILE_PATH
+
+echo $(ls)
 
 az iot edge deployment create \
     -d ${DEPLOYMENT_VERSION} \
