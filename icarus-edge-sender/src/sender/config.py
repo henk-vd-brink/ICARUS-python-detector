c9e232d3887b148900e5309a99ba9c94080d27da
Henk <hcebrink@gmail.com>
2022-12-02 08:47:14 +0100

    updated json

--- icarus-edge-sender/src/sender/config.py
+++ icarus-edge-sender/src/sender/config.py
@@ -1,7 +1,7 @@
 def get_mq_client_configuration():
     return dict(
         broker_ip_address="192.168.178.47",
-        broker_port=5671,
+        broker_port=5672,
         broker_username="guest",
         broker_password="guest",
     )
