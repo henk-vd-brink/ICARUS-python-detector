7ce27ee7d07c264615da5861d90fe431db69aa14
Prominendt <hcebrink@gmail.com>
2022-12-18 10:04:03 +0100

    test PL

--- icarus-edge-sender/src/sender/config.py
+++ icarus-edge-sender/src/sender/config.py
@@ -1,18 +1,24 @@
+import os
+
+
 def get_rabbit_mq_client_configuration():
     return dict(
-        broker_ip_address="192.168.178.47",
-        broker_port=5672,
-        broker_username="guest",
-        broker_password="guest",
+        broker_ip_address=os.environ.get("REMOTE_IP_ADDRESS"),
+        broker_port=os.environ.get("REMOTE_RABBITMQ_PORT"),
+        broker_username=os.environ.get("REMOTE_RABBITMQ_USERNAME"),
+        broker_password=os.environ.get("REMOTE_RABBITMQ_PASSWORD"),
     )
 
 
 def get_redis_mq_client_configuration():
-    return dict(redis_ip_address="icarus-edge-redis", port=6379)
+    return dict(redis_ip_address=os.environ.get("REDIS_IP_ADDRESS"), port=6379)
 
 
 def get_remote_host_configuration():
-    return dict(remote_host_ip_address="192.168.178.47", remote_host_port=8443)
+    return dict(
+        remote_host_ip_address=os.environ.get("REMOTE_IP_ADDRESS"),
+        remote_host_port=os.environ.get("REMOTE_HOST_PORT"),
+    )
 
 
 def get_file_system_configuration():
--- scripts/build_and_deploy_to_iotedge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BUILD_VERSION="$(date +%Y%m%d-%H%M)"
-DEPLOYMENT_VERSION="dev-$BUILD_VERSION"
-
-./scripts/build_and_push.sh $BUILD_VERSION
-
-./scripts/deploy_to_iotedge.sh $BUILD_VERSION $DEPLOYMENT_VERSION
