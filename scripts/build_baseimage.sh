6b3f8b5422fff300b535ecb924e02c490cfbabcd
Prominendt <hcebrink@gmail.com>
2023-01-22 09:48:22 +0100

    Removed contrib from docker opencv

--- scripts/build_baseimage.sh
+++ scripts/build_baseimage.sh
@@ -2,5 +2,7 @@
 
 export $(grep -v '^#' .env.example | xargs)
 
+BUILD_VERSION=$(date '+%Y-%m-%d')
+
 docker buildx build \
-    -t ${CONTAINER_REGISTRY}/icarus-edge-baseimage:${BUILD_VERSION:-latest} docker
+    -t ${CONTAINER_REGISTRY}/icarus-edge-baseimage:${BUILD_VERSION:-latest} docker --push
