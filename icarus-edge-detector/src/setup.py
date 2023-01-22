7c4a49ef74693bc9068c6a5955934e9d5d5b363b
Prominendt <hcebrink@gmail.com>
2023-01-22 09:31:21 +0100

    Removed contrib from docker opencv

--- icarus-edge-detector/src/setup.py
+++ icarus-edge-detector/src/setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+
 setup(
     name="ICARUS Edge detector",
     version="0.1.0",
     packages=find_packages(include=["detector"]),
+    install_requires=required_packages,
 )
